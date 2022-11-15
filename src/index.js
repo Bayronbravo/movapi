a290144d818496626b56a80594ebce74f0a3f7be
Bayron Bravo <bayron.bravo99@gmail.com>
2022-11-15 17:49:10 -0300

    final

--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ const movieRoutes = require("./routes/movie");
 
 const app = express();
 const port = process.env.PORT || 9000;
-
-
+console.log("====================");
+console.log("Database_URL", process.env.DATABASE_URL);
+console.log("====================");
 //coneccion a mongobd
 mongoose
     mongoose.connect(process.env.MONGODB_URI)
     .then(()=> console.log("Conected"))
     .catch((error) => console.error(error));
-    
+
 //middleware
 app.use(express.json());
 app.use('/api', movieRoutes);
